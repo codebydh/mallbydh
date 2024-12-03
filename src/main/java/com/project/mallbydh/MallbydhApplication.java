b1c7ac5b581512e5f42a905bd97e55fdadb6526b
dh <codebydh@gmail.com>
2024-12-03 15:36:37 +0900

    First commit - (basic configuration)

---
+++ src/main/java/com/project/mallbydh/MallbydhApplication.java
@@ -0,0 +1,13 @@
+package com.project.mallbydh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallbydhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallbydhApplication.class, args);
+    }
+
+}
